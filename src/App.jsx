0d03d55997df5de167b54bbd76044f30e9b60567
Hatsune02 <tzunundonavin@gmail.com>
2025-04-08 21:49:25 -0600

    Creando el carrito y usando el contexto para conectarlo con toda la aplicacion

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,24 @@ import { products as initialProducts } from "./mocks/products.json"
 import { Products } from "./components/Products"
 import { Header } from "./components/Header"
 import { Footer } from "./components/Footer"
-import { useState } from "react"
 import { useFilters } from "./hooks/useFilters"
+import { Cart } from "./components/Cart"
+import { CartProvider } from "./context/cart"
 
 
 
 function App() {
   const { filterProducts } = useFilters()
-  
+
   const filteredProducts = filterProducts(initialProducts)
 
   return (
-    <>
+    <CartProvider>
       <Header />
+      <Cart />
       <Products products={filteredProducts} />
       <Footer></Footer>
-    </>
+    </CartProvider>
   )
 }
 
