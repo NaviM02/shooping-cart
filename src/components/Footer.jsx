2bbd3785178bb7d3b7714ad527fc012ab1000ab2
Hatsune02 <tzunundonavin@gmail.com>
2025-04-08 20:24:06 -0600

    Creando footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import "./Footer.css"
+
+export function Footer() {
+    return (
+        <footer className="footer">
+            <h4>Proyecto personal React ⚛️ － <span>@NaviM02</span></h4>
+            <h5>Shopping Cart</h5>
+        </footer>
+    )
+}
