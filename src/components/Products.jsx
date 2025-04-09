f595c2f2ba47754b8b2efa9d86c815b22f26f170
Hatsune02 <tzunundonavin@gmail.com>
2025-04-08 21:49:40 -0600

    Agregando funcionalidad para remover objeto desde los productos

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,27 +1,47 @@
 import './Products.css'
-import { AddToCartIcon } from './Icons'
+import { AddToCartIcon, RemoveFromCartIcon } from './Icons'
+import { useCart } from '../hooks/useCart'
 
 export function Products({ products }){
+    const { addToCart, removeFromCart, cart } = useCart()
+
+    const checkProductInCart = product => {
+        return cart.some(item => item.id === product.id)
+    }
 
     return(
         <main className='products'>
             <ul>
-                {
-                    products.map(product => (
+                {products.map(product => {
+                    const isProductInCart = checkProductInCart(product)
+                    
+                    return (
                         <li key={product.id}>
-                            <img 
-                                src={product.image}
-                                alt={product.title}
-                            />
-                            <div>
-                                <strong>{product.title}</strong> - ${product.price}
-                            </div>
-                            <div>
-                                <AddToCartIcon></AddToCartIcon>
-                            </div>
-                        </li>
-                    ))
-                }
+                        <img 
+                            src={product.image}
+                            alt={product.title}
+                        />
+                        <div>
+                            <strong>{product.title}</strong> - ${product.price}
+                        </div>
+                        <div>
+                            <button 
+                                style={{backgroundColor: isProductInCart ? 'red' : '#09f' }} 
+                                onClick={() => {
+                                    isProductInCart 
+                                    ? removeFromCart(product)
+                                    : addToCart(product)
+                            }}>
+                                {
+                                    isProductInCart
+                                    ? <RemoveFromCartIcon />
+                                    : <AddToCartIcon />
+                                }
+                            </button>
+                        </div>
+                    </li>
+                    )
+                    })}
             </ul>
         </main>
     )
